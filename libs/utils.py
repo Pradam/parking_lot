3355ec7ee5ce73fb717c269efc57f322366ec1fa
pradam <pradamabhilash@gmail.com>
2019-03-02 13:01:50 +0530

    initial project directory setup

---
+++ libs/utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+parking_lot.utils
+~~~~~~~~~~~~~~
+This module provides utility functions that are used within Parking Lot module
+that are also useful for external consumption.
+"""
+
